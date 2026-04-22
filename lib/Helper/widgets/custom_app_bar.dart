a0e9e3228912be88ca32d09720dbcb820ef24d74
khalid-zsh <eyeshotkhalid@gmail.com>
2026-04-22 18:06:05 +0600

    8th Commit

--- lib/Helper/widgets/custom_app_bar.dart
+++ lib/Helper/widgets/custom_app_bar.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-PreferredSizeWidget CustomAppBar (){
+PreferredSizeWidget CustomAppBar ({
+  required BuildContext context,
+  required String title,
+}){
   return AppBar(
-
+    leading: IconButton(
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        icon: Icon(Icons.arrow_back_ios_new)
+    ),
+    title: Text(
+      title,
+    style: TextStyle(
+      fontWeight: FontWeight.w500,
+    ),
+    ),
+    centerTitle: true,
   );
 }
--- lib/view/CourseDetailsView/course_details_view.dart
+++ lib/view/CourseDetailsView/course_details_view.dart
@@ -1,8 +1,12 @@
+import 'package:e_learning_ui/Helper/widgets/custom_app_bar.dart';
+import 'package:e_learning_ui/Utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class CourseDetailsView extends StatefulWidget {
 
   final String title;
+
 
   const CourseDetailsView({super.key, required this.title});
 
@@ -11,10 +15,266 @@ class CourseDetailsView extends StatefulWidget {
 }
 
 class _CourseDetailsViewState extends State<CourseDetailsView> {
+
+
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.asset("assets/videos/video.mp4")
+      ..initialize().then((_){
+        setState(() {
+          _controller.play();
+        });
+      });
+
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
 
+    final size = MediaQuery.sizeOf(context);
+
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: CustomAppBar(
+            context: context,
+            title: widget.title
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  alignment: Alignment.center,
+                  height: size.height * .2,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withValues(alpha: .1),
+                            blurRadius: 5,
+                            spreadRadius: .5
+                        )
+                      ]
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * .25,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        // Video
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: _controller.value.isInitialized
+                              ? SizedBox.expand(
+                            child: FittedBox(
+                              fit: BoxFit.cover,
+                              child: SizedBox(
+                                width: _controller.value.size.width,
+                                height: _controller.value.size.height,
+                                child: VideoPlayer(_controller),
+                              ),
+                            ),
+                          )
+                              : const CircularProgressIndicator(),
+                        ),
+
+                        // Play/Pause button
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _controller.value.isPlaying
+                                  ? _controller.pause()
+                                  : _controller.play();
+                            });
+                          },
+                          child: CircleAvatar(
+                            backgroundColor: Colors.black54,
+                            child: Icon(
+                              _controller.value.isPlaying
+                                  ? Icons.pause
+                                  : Icons.play_arrow,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+              ),
+              SizedBox(height: 12),
+              Text(
+                  "Career With Artificial Intelligence",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Created By,",
+                    style: TextStyle(
+                      color: Colors.black.withValues(alpha: .5),
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    "Eyeshot Khalid",
+                    style: TextStyle(
+                      color: AppColors.primaryTextColor
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  Icon(Icons.star_border, color: Colors.black.withValues(alpha: .5),),
+                  SizedBox(width: 5),
+                  Text(
+                    "4.8",
+                    style: TextStyle(
+                      color: Colors.black.withValues(alpha: .5),
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Icon(Icons.access_time_sharp, color: Colors.black.withValues(alpha: .5),),
+                  SizedBox(width: 5),
+                  Text(
+                    "50 Hours",
+                    style: TextStyle(
+                      color: Colors.black.withValues(alpha: .5),
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    "\$50",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade100,
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                  child: TabBar(
+                    indicatorSize: TabBarIndicatorSize.tab,
+                      indicator: BoxDecoration(
+                        color: AppColors.primaryColor,
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      labelColor: Colors.white,
+                      unselectedLabelColor: Colors.black54,
+                      dividerColor: Colors.transparent,
+                      tabs: [
+                        Tab(text: "Playlist (22)"),
+                        Tab(text: "Description"),
+                  ]
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    ListView(
+                      children: [
+                        _buildLessonItem("Introduction", "2 Min 18 Sec", true, true),
+                        _buildLessonItem("What is Artificial Intelligence?", "18 Min 46 Sec", false, false),
+                        _buildLessonItem("How to make wireframe", "20 Min 58 Sec", false, false),
+                        _buildLessonItem("Your first design", "25 Min 10 Sec", false, false),
+                      ],
+                    ),
+                    const Center(child: Text("Course Description Here")),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Padding(
+            padding: EdgeInsetsGeometry.fromLTRB(15, 0, 15, 20),
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: Colors.amber.shade400,
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: Icon(Icons.bookmark_border, color: Colors.white,),
+                  ),
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                    child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primaryColor,
+                          minimumSize: Size(double.infinity, 60),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15))
+                        ),
+                        child: Text(
+                          "Enroll Now",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        )
+                    )
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+Widget _buildLessonItem(String title, String duration, bool isCompleted, bool isUnlocked) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8.0),
+    child: ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: CircleAvatar(
+        backgroundColor: AppColors.primaryColor.withValues(alpha: .2),
+        child: Icon(Icons.play_arrow, color: AppColors.primaryColor),
+      ),
+      title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text(duration, style: TextStyle(color: Colors.grey.shade500)),
+      trailing: isCompleted
+          ? const Icon(Icons.check_circle, color: Colors.green)
+          : Icon(Icons.lock, color: Colors.amber.shade200),
+    ),
+  );
 }
